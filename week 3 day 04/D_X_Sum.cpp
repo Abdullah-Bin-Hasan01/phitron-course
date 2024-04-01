d5bb478f8322f2e86b19e006ad2e73b4719d63a4
Abdullah-Bin-Hasan01 <163655593+Abdullah-Bin-Hasan01@users.noreply.github.com>
2024-04-01 23:25:41 +0600

    Add files via upload
    e

---
+++ week 3 day 04/D_X_Sum.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int ar[n][m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> ar[i][j];
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int sum = 0;
+                sum += ar[i][j];
+                int x = i - 1, y = j - 1;
+                while (x >= 0 && y >= 0)
+                {
+                    sum += ar[x][y];
+                    x--;
+                    y--;
+                }
+                x = i - 1;
+                y = j + 1;
+                while (x >= 0 && y < m)
+                {
+                    sum += ar[x][y];
+                    x--;
+                    y++;
+                }
+                x = i + 1;
+                y = j - 1;
+                while (x < n && y >= 0)
+                {
+                    sum += ar[x][y];
+                    x++;
+                    y--;
+                }
+                x = i + 1;
+                y = j + 1;
+                while (x < n && y < m)
+                {
+                    sum += ar[x][y];
+                    x++;
+                    y++;
+                }
+                ans = max(ans, sum);
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
